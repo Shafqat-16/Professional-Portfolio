95defee8a31d9c618bd2218095d1cc9d6ecfc545
Shafqat-16 <m.shafqat1613.com>
2026-02-02 23:02:24 +0500

    email notificication optimization for seo

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// Custom Cursor
-const cursorDot = document.querySelector(".cursor-dot");
-const cursorOutline = document.querySelector(".cursor-outline");
-
-window.addEventListener("mousemove", (e) => {
-    const posX = e.clientX;
-    const posY = e.clientY;
-
-    cursorDot.style.left = `${posX}px`;
-    cursorDot.style.top = `${posY}px`;
-
-    cursorOutline.animate({
-        left: `${posX}px`,
-        top: `${posY}px`
-    }, { duration: 500, fill: "forwards" });
-});
-
 // Tabbed Navigation Logic
 const sections = document.querySelectorAll('section');
 const navLinks = document.querySelectorAll('.nav-links a');
